fc51d94817ba026cd6bb076f4ff9e5b718d5e346
Thiago Souza <dev@thiagosouza.com>
2023-01-11 15:20:07 -0300

    chore: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export function App() {
+  return (
+    <main className="h-screen w-full flex items-center justify-center">
+      <div className="w-[54rem] h-[32rem] grid grid-cols-2 gap-4 bg-violet-200 rounded-lg">
+
+      </div>
+    </main>
+  )
+}
---
+++ src/data/product.ts
@@ -0,0 +1,19 @@
+interface Product {
+  id: number
+  name: string
+  value: number
+  image: {
+    filePath: string
+    file3dPath: string
+  }
+}
+
+export const product: Product = {
+  id: 42404,
+  name: 'Sofá Margot II - Rosé',
+  value: 4000,
+  image: {
+    filePath: '',
+    file3dPath: '',
+  }
+}
